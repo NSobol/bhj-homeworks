c9099af0a52bef915bf4ce6f94c7e3485103380c
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-04-27 09:54:56 +0300

    Внесены правки

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -8,6 +8,7 @@ modal.classList.add("modal_active");
 const showSuccess = document.getElementsByClassName("show-success");
 const btn = showSuccess.item(0);
 btn.onclick = function () {
+    this.parentElement.parentElement.classList.remove("modal_active");
     modalSuccess.classList.add("modal_active");
 }
 
