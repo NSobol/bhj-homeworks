92b4a45ae8b8cd8dedef71b48e5fa6293129763c
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-04-29 08:51:47 +0300

    Вторая задача

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,1 +1,12 @@
 "use strict"
+const menuList = document.querySelectorAll("ul.menu_main > li> a");
+let arrayMenuList = Array.from(menuList);
+
+for (let i = 0; i < arrayMenuList.length; i++) {
+    if (arrayMenuList[i].nextElementSibling) {
+        arrayMenuList[i].onclick = function () {
+            arrayMenuList[i].nextElementSibling.classList.toggle("menu_active");
+            return false;
+        }
+    }
+}
