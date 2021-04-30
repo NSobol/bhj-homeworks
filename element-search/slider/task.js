28b3d6fa727592ca86ea03773013c56c40ca5db3
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-04-30 23:01:34 +0300

    Третья задача

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,28 +3,17 @@ const sliderPrev = document.querySelector(".slider__arrow_prev");
 const sliderNext = document.querySelector(".slider__arrow_next");
 const sliders = document.querySelectorAll(".slider__item");
 let slider = Array.from(sliders);
-let sliderLength = slider.length;
 
 sliderPrev.onclick = function () {
-    let slideIndex = slider.findIndex(elem => {
-        if (elem.classList.value === "slider__item slider__item_active") {
-            return true;
-        }
-        return false;
-    });
+    let slideIndex = slider.findIndex(elem => elem.classList.contains("slider__item_active"));
     slider[slideIndex].classList.toggle("slider__item_active");
-    slideIndex === 0 ? slideIndex = sliderLength - 1 : slideIndex -= 1;
+    slideIndex === 0 ? slideIndex = slider.length - 1 : slideIndex -= 1;
     slider[slideIndex].classList.toggle("slider__item_active");
 }
 
 sliderNext.onclick = function () {
-    let slideIndex = slider.findIndex(elem => {
-        if (elem.classList.value === "slider__item slider__item_active") {
-            return true;
-        }
-        return false;
-    });
+    let slideIndex = slider.findIndex(elem => elem.classList.contains("slider__item_active"));
     slider[slideIndex].classList.toggle("slider__item_active");
-    slideIndex === sliderLength - 1 ? slideIndex = 0 : slideIndex += 1;
+    slideIndex === slider.length - 1 ? slideIndex = 0 : slideIndex += 1;
     slider[slideIndex].classList.toggle("slider__item_active");
 }
