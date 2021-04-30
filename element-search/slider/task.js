2d970287a85622474c53490646b8b4b7dc6d07bf
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-04-30 19:21:00 +0300

    Третья задача

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,32 +1,30 @@
 "use strict"
-let currentSlide = 0;
 const sliderPrev = document.querySelector(".slider__arrow_prev");
 const sliderNext = document.querySelector(".slider__arrow_next");
 const sliders = document.querySelectorAll(".slider__item");
 let slider = Array.from(sliders);
 let sliderLength = slider.length;
-console.log(slider);
 
 sliderPrev.onclick = function () {
-    if (currentSlide == 0) {
-        slider[currentSlide].classList.toggle("slider__item_active");
-        currentSlide = sliderLength - 1;
-        slider[currentSlide].classList.toggle("slider__item_active");
-    } else {
-        currentSlide -= 1;
-        slider[currentSlide].classList.toggle("slider__item_active");
-    }
+    let slideIndex = slider.findIndex(elem => {
+        if (elem.classList.value === "slider__item slider__item_active") {
+            return true;
+        }
+        return false;
+    });
+    slider[slideIndex].classList.toggle("slider__item_active");
+    slideIndex === 0 ? slideIndex = sliderLength - 1 : slideIndex -= 1;
+    slider[slideIndex].classList.toggle("slider__item_active");
 }
 
 sliderNext.onclick = function () {
-    if (currentSlide >= 0) {
-        slider[currentSlide].classList.toggle("slider__item_active");
-        if (currentSlide === sliderLength - 1) {
-            currentSlide = 0;
-            slider[currentSlide].classList.toggle("slider__item_active");
-        } else {
-            currentSlide += 1;
-            slider[currentSlide].classList.toggle("slider__item_active");
+    let slideIndex = slider.findIndex(elem => {
+        if (elem.classList.value === "slider__item slider__item_active") {
+            return true;
         }
-    }
+        return false;
+    });
+    slider[slideIndex].classList.toggle("slider__item_active");
+    slideIndex === sliderLength - 1 ? slideIndex = 0 : slideIndex += 1;
+    slider[slideIndex].classList.toggle("slider__item_active");
 }
