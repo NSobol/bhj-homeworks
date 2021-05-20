a7ed4835fbacc17caefd9f9100dd5b480bc8322f
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-05-20 15:14:32 +0300

    Внесены исправления во вторую задачу

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,25 +2,21 @@
 const btnsMinus = document.querySelectorAll('.product__quantity-control_dec');
 const btnsPlus = document.querySelectorAll('.product__quantity-control_inc');
 const btnsBasket = document.querySelectorAll('.product__add');
-let productValue = document.querySelector(".product__quantity-value");
-console.log(btnsMinus);
+let product;
+//console.log(btnsMinus);
 
 for (let btn of btnsMinus) {
-    btn.addEventListener('click', () => {
-        let product = document.querySelector(".product__quantity-value");
+    btn.addEventListener('click', (event) => {
+        findProduct(event);
         let productValue = product.textContent;
-        if (productValue > 1) {
-            productValue--;
-        } else {
-            productValue = 1;
-        }
+        productValue > 1 ? productValue-- : productValue = 1;
         product.innerText = productValue;
     });
 }
 
 for (let btn of btnsPlus) {
-    btn.addEventListener('click', () => {
-        let product = document.querySelector(".product__quantity-value");
+    btn.addEventListener('click', (event) => {
+        findProduct(event);
         let productValue = product.textContent;
         if (productValue => 1) {
             productValue++;
@@ -29,10 +25,14 @@ for (let btn of btnsPlus) {
     });
 }
 
+function findProduct(event) {
+    let currentProduct = event.target;
+    let productColntrols = currentProduct.closest(".product__quantity-controls");
+    let controls = productColntrols.childNodes;
+    return product = controls[3];
+}
 for (let btn of btnsBasket) {
     btn.addEventListener('click', () => {
 
     });
 }
-
-function 
