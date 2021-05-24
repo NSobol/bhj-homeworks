3c7c2a10c5ae5cebd2953e6adc38b9ffeee77e17
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-05-24 18:29:18 +0300

    Внесены исправления в третью задачу

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,38 +1,58 @@
 "use strict"
-const btnsMinus = document.querySelectorAll('.product__quantity-control_dec');
-const btnsPlus = document.querySelectorAll('.product__quantity-control_inc');
-const btnsBasket = document.querySelectorAll('.product__add');
-let product;
-//console.log(btnsMinus);
+const cart = document.querySelector('.cart__products');
+const btnsChange = document.querySelectorAll('.product__quantity-control');
+const addButtons = document.querySelectorAll('.product__add');
 
-for (let btn of btnsMinus) {
-    btn.addEventListener('click', (event) => {
-        findProduct(event);
-        let productValue = product.textContent;
-        productValue > 1 ? productValue-- : productValue = 1;
-        product.innerText = productValue;
-    });
+
+for (let item of btnsChange) {
+    item.addEventListener('click', change);
 }
 
-for (let btn of btnsPlus) {
-    btn.addEventListener('click', (event) => {
-        findProduct(event);
-        let productValue = product.textContent;
-        if (productValue => 1) {
-            productValue++;
+for (let item of addButtons) {
+    item.addEventListener('click', addCart);
+}
+
+function change(event) {
+    let value = event.target.parentNode.querySelector('.product__quantity-value');
+    let count = +value.innerText;
+
+    if (event.target.classList.contains('product__quantity-control_inc')) {
+        count++;
+        value.innerText = count;
+    } else {
+        if (count > 1) {
+            count--;
+            value.innerText = count;
+        } else {
+            value.innerText = 1;
         }
-        product.innerText = productValue;
-    });
+    }
 }
 
-function findProduct(event) {
-    let currentProduct = event.target;
-    let productColntrols = currentProduct.closest(".product__quantity-controls");
-    let controls = productColntrols.childNodes;
-    return product = controls[3];
-}
-for (let btn of btnsBasket) {
-    btn.addEventListener('click', (event) => {
-        findProduct(event);
-    });
+function addCart(event) {
+
+    const product = event.target.closest('.product');
+    const id = product.dataset.id;
+    const countFromProduct = +event.target.parentNode.querySelector('.product__quantity-value').innerText;
+
+    for (let item of cart.children) {
+
+        if (item.dataset.id === id) {
+            let productCount = item.querySelector('.cart__product-count');
+            let total = +productCount.innerText;
+            productCount.innerText = total + countFromProduct;
+
+            return false;
+        }
+    }
+
+    const productImg = product.querySelector('.product__image').src;
+    const count = product.querySelector('.product__quantity-value').innerText;
+
+    const productCart = `<div class="cart__product" data-id="${id}">
+                                <img class="cart__product-image" src="${productImg}">
+                                <div class="cart__product-count">${count}</div>
+                            </div>`;
+
+    cart.insertAdjacentHTML('beforeend', productCart);
 }
