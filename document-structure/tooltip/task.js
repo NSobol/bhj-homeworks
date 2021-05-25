70320c9b0e78741809e22da7e70561f4ba9e4c2e
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-05-25 09:17:28 +0300

    Внесены новые исправления в первую задачу

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -6,24 +6,17 @@ for (let link of tooltips) {
     link.addEventListener('click', getHint);
 }
 
-hint.addEventListener('click', () => hint.classList.toggle('tooltip_active'));
-
 function getHint(event) {
     event.preventDefault();
     const tooltip = document.querySelector('.tooltip_active');
-
-    if (tooltip) {
-        tooltip.classList.toggle('tooltip_active');
-    }
     let activ = event.target;
     let hintText = activ.getAttribute('title');
     if (hintText === hint.innerText) {
-        hint.classList.remove("tooltip_active");
-    } else {
-        hint.innerText = hintText;
         hint.classList.toggle("tooltip_active");
-        hint.style.top = activ.getBoundingClientRect().top + 20 + 'px';
-        hint.style.left = activ.getBoundingClientRect().left + 'px';
+    } else {
+        hint.classList.add("tooltip_active");
     }
-
+    hint.innerText = hintText;
+    hint.style.top = activ.getBoundingClientRect().top + 20 + 'px';
+    hint.style.left = activ.getBoundingClientRect().left + 'px';
 }
